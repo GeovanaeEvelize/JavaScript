69ca89c4bce0365385d50bf6ae42acb13b672534
Suzuki <114406587+GeovanaeEvelize@users.noreply.github.com>
2023-11-17 08:30:08 -0300

    blz

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (idElementoAudio){
 
     document.querySelector(idElementoAudio).play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0
-
-//enquanto
-while (contador < listaDeTeclas.length) {
-
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`;
-
-    console.log(idAudio);
-
-    tecla.onclick = function () {
-        tocaSom(idAudio)
+    
+    
     }
-
-    contador = contador + 1;
-
-    console.log(contador)
-}
+    
+    
+    
+    
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+    
+    let contador = 0;
+    
+    for (let contador = 0;contador < listaDeTeclas.length; contador++) {
+    
+         const tecla = listaDeTeclas[contador];
+         const instrumento = tecla.classList[1];
+     
+         const idAudio = `#som_${instrumento}`;
+     
+         tecla.onclick = function () {
+     
+             tocaSom (idAudio);
+     
+         }
+            tecla.onkeydown = function (evento){
+                console.log(evento.code)
+    
+                if ('Space') {
+               tecla.classList.add ('ativa');
+    
+                }
+    
+            }
+            tecla.onkeyup = function (){
+                tecla.classList.remove('ativa');
+           
+           
+            }
+        }
+    
